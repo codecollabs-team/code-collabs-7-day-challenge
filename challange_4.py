4e0811941b26e0ee8a0dc0b1311dba1ba537a1e7
barwarski <63984342+barwarski@users.noreply.github.com>
2020-04-27 19:25:53 +0200

    Awien Barwari

--- challange_4.py
+++ challange_4.py
@@ -1,5 +1,14 @@
 def mySort(a):
     sortedArray = a
-    for x in a:
-        if a[x] > a[x+1]:
-            sortedArray[x] = a[x]
+    x = 0
+    while x < (len(a)-1):
+        if sortedArray[x] > sortedArray[x+1]:
+            copy = sortedArray[x]
+            sortedArray[x] = sortedArray[x+1]
+            sortedArray[x+1] = copy
+            x += 1
+        else:
+            x += 1
+    return sortedArray
+
+print(mySort([1,3,60,4,77,500]))
